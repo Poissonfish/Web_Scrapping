ec2b50d07584c2f74b907ef6cd5cea9825cfe020
Poissonfish <b99612021@ntu.edu.tw>
2016-02-21 13:50:51 +0800

    New Folder: iPhone6s_Price_Trend

--- iPhone6s_Price_Trend/photobuy.R
+++ iPhone6s_Price_Trend/photobuy.R
@@ -42,7 +42,7 @@ for (i in 1:length(index)){
 }
 length(subindex)
 
-data=data.frame(Title=NA, Date=NA, Prize=NA, URL=NA)
+data=data.frame(Title=NA, Date=NA, Price=NA, URL=NA)
 for (i in 1:length(subindex)){
   URL=subindex[i]
   res_text = GET(URL) %>% 
@@ -89,8 +89,8 @@ dim(select)
 #select=data
 select$Date=gsub('時間','',select$Date) %>%
   strptime(.,'%a %b %e %H:%M:%S %Y')
-select$Prize=as.numeric(gsub('[^0-9]','',select$Prize))
-select=select[select$Prize>10000&select$Prize<40000,]
+select$price=as.numeric(gsub('[^0-9]','',select$price))
+select=select[select$price>10000&select$price<40000,]
 dim(select)
 
 #select=read.csv('iphone6s.csv', encoding = 'UTF-8' )
@@ -111,21 +111,21 @@ i128=select[grep('128',select[,1]),];dim(i128)
  #i16=i16[-c(57,59),]
  #i64=i64[-(641),]
 
-reg=subset(i64, select=c(Prize,D.value))
-model=lm(Prize ~ D.value, data = reg)
-reg=subset(fortify(model),select=c(Prize,D.value,.fitted))
+reg=subset(i64, select=c(price,D.value))
+model=lm(price ~ D.value, data = reg)
+reg=subset(fortify(model),select=c(price,D.value,.fitted))
 names(reg)[3]='FIT'
 i64=  join(i64,reg);head(i64)
 
-reg=subset(i128, select=c(Prize,D.value))
-model=lm(Prize ~ D.value, data = reg)
-reg=subset(fortify(model),select=c(Prize,D.value,.fitted))
+reg=subset(i128, select=c(price,D.value))
+model=lm(price ~ D.value, data = reg)
+reg=subset(fortify(model),select=c(price,D.value,.fitted))
 names(reg)[3]='FIT'
 i128=  join(i128,reg);head(i128)
 
-reg=subset(i16, select=c(Prize,D.value))
-model=lm(Prize ~ D.value, data = reg)
-reg=subset(fortify(model),select=c(Prize,D.value,.fitted))
+reg=subset(i16, select=c(price,D.value))
+model=lm(price ~ D.value, data = reg)
+reg=subset(fortify(model),select=c(price,D.value,.fitted))
 names(reg)[3]='FIT'
 i16=  join(i16,reg);head(i16)
 
@@ -135,7 +135,7 @@ dim(i64)
 
 # 
 # library(ggplot2)
-# g=ggplot(data=select,aes(x=Date,y=Prize,group=1))+geom_point(colour='red',alpha=.8)+stat_smooth(method='glm')+geom_line()+
+# g=ggplot(data=select,aes(x=Date,y=price,group=1))+geom_point(colour='red',alpha=.8)+stat_smooth(method='glm')+geom_line()+
 #   scale_y_continuous(breaks=seq(0,35000,1000),limits=c(10000,35000))
 # g+scale_x_datetime(breaks="2 weeks")
 # 
@@ -147,9 +147,9 @@ i16$D2=as.numeric(i16$Date)*1000
 i64$D2=as.numeric(i64$Date)*1000
 i128$D2=as.numeric(i128$Date)*1000
 
-p16=subset(i16,select=c('D2','Prize','Title','URL'))
-p64=subset(i64,select=c('D2','Prize','Title','URL'))
-p128=subset(i128,select=c('D2','Prize','Title','URL'))
+p16=subset(i16,select=c('D2','price','Title','URL'))
+p64=subset(i64,select=c('D2','price','Title','URL'))
+p128=subset(i128,select=c('D2','price','Title','URL'))
 
 l16=subset(i16,select=c('D2','FIT'))
 l64=subset(i64,select=c('D2','FIT'))
@@ -172,7 +172,7 @@ hm$series(data = toJSONArray2(p128, json = F,names=T), name = "128G",type = c("s
 hm$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)',
           'rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
 hm$plotOptions(column = list(stacking = "normal", pointPadding = 0, groupPadding = 0, borderWidth = 0))
-hm$tooltip(borderWidth=0, followPointer=TRUE, followTouchMove=TRUE, shared = FALSE,formatter = "#! function(){return this.point.z + '<br>'+ 'Prize:' + this.point.y ;} !#")
+hm$tooltip(borderWidth=0, followPointer=TRUE, followTouchMove=TRUE, shared = FALSE,formatter = "#! function(){return this.point.z + '<br>'+ 'price:' + this.point.y ;} !#")
 hm$plotOptions(
   scatter = list(
     cursor = "pointer", 
@@ -191,4 +191,4 @@ hm$xAxis(type = 'datetime', labels = list(
 hm$save(paste('iphone6s_',Sys.Date(),'.html',sep=''),standalone=TRUE)
 browseURL(paste('iphone6s_',Sys.Date(),'.html',sep=''))
 hm$publish(paste('iphone6s_',Sys.Date(),'.html',sep=''), host ='rpubs')
-#hm$publish('iPhone6s_PrizeTrend', host ='rpubs', user='Poissonfish')
+#hm$publish('iPhone6s_priceTrend', host ='rpubs', user='Poissonfish')
